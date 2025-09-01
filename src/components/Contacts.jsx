6c6cdccf1be91f4b2c778b18fb351ea2613fdc06
IvanMaieli <ivan.maieli@gmail.com>
2025-09-02 00:38:47 +0200

    up

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -2,7 +2,7 @@ const Contacts = () => {
   return (
     <div className="space-y-2">
       <p>Email: <a href="mailto:ivan.maieli@gmail.com" className="underline text-green-200">ivan.maieli@gmail.com</a></p>
-      <p>GitHub: <a href="https://github.com/IvanMaieli" target="_blank" className="underline text-green-200">github.com/username</a></p>
+      <p>GitHub: <a href="https://github.com/IvanMaieli" target="_blank" className="underline text-green-200">github.com/IvanMaieli</a></p>
       <p>LinkedIn: <a href="https://linkedin.com/in/ivan-maieli" target="_blank" className="underline text-green-200">linkedin.com/in/ivan-maieli</a></p>
       <p>Instagram: <a href="https://www.instagram.com/ivan.maieli/" target="_blank" className="underline text-green-200">https://www.instagram.com/ivan.maieli/</a></p>
 
